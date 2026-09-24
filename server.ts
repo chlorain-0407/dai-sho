76678077397385d9c20ee86d6a12927d7b6d0e94
chlorain-0407 <chlorain@gmail.com>
2026-09-24 18:39:20 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { apiRouter } from './src/server/routes';
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT;
 
   // JSON Body Parser
   app.use(express.json());
